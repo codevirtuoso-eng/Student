648dabadb8d52e13fc1ffe028f486e104d3c5c6b
codevirtuoso-eng <donclasshelp@gmail.com>
2025-05-16 02:39:03 +0300

    Update Startup.cs

--- MvcWebApplication/Startup.cs
+++ MvcWebApplication/Startup.cs
@@ -6,140 +6,180 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using MvcWebApplication.ViewFunctions;
-using System.Net.Http;
+using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using WebAppAPI.ApiFunctions;
 
-namespace MvcWebApplication
+namespace WebAppAPI
 {
-	public class Startup
-	{
-		private IWebHostEnvironment _env;
-		private string _contentRootPath;
+    public class Startup
+    {
+        private IWebHostEnvironment _env;
+        private string _contentRootPath;
 
-		public Startup(IConfiguration configuration, IWebHostEnvironment env)
-		{
-			Configuration = configuration;
-			_env = env;
-			_contentRootPath = env.ContentRootPath;
-		}
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            _env = env;
+            _contentRootPath = env.ContentRootPath;
+        }
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			// JWT authentication configuration
-			services.AddAuthentication(x =>
-			{
-				x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-				x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-				x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-				x.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
-			}).AddJwtBearer(options =>
-			{
-				options.SaveToken = true;  // Saves the JWT access token in the current HttpContext
-				options.RequireHttpsMetadata = false;
-				options.TokenValidationParameters = new TokenValidationParameters
-				{
-					ValidateIssuer = true,
-					ValidateAudience = true,
-					ValidateLifetime = true,
-					ValidateIssuerSigningKey = true,
-					ValidIssuer = Configuration["JWT:Issuer"],
-					ValidAudience = Configuration["JWT:Audience"],
-					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
-				};
-				options.Events = new JwtBearerEvents
-				{
-					OnMessageReceived = context =>
-					{
-						// option to extract access token from incoming request cookies
-						if (context.Request.Cookies.ContainsKey("X-Access-Token"))
-						{
-							context.Token = context.Request.Cookies["X-Access-Token"];
-							if (!context.Request.Headers.ContainsKey("Authorization"))
-							{
-								context.Request.Headers.Add("Authorization", "Bearer " + context.Token);
-							}
-						}
-						return Task.CompletedTask;
-					}
-				};
-			})
-			.AddCookie(options =>
-			{
-				options.Cookie.SameSite = SameSiteMode.Strict;
-				options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-				options.Cookie.IsEssential = true;
-				options.SlidingExpiration = true;
-			});
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // JWT authentication configuration
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true; // Saves the JWT access token in the current HttpContext
+                options.RequireHttpsMetadata = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["JWT:Issuer"],
+                    ValidAudience = Configuration["JWT:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        // option to extract access token from incoming request cookies
+                        if (context.Request.Cookies.ContainsKey("X-Access-Token"))
+                        {
+                            context.Token = context.Request.Cookies["X-Access-Token"];
+                            if (!context.Request.Headers.ContainsKey("Authorization"))
+                            {
+                                context.Request.Headers.Add("Authorization", "Bearer " + context.Token);
+                            }
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            })
+            .AddCookie(options =>
+            {
+                options.Cookie.SameSite = SameSiteMode.Strict;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.Cookie.IsEssential = true;
+                options.SlidingExpiration = true;
+            });
 
-			// Definition for HttpClient using 
-			services.AddHttpClient("LocalClient")
-			.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-			{
-				UseCookies = false // tells web API to not store cookies
-			});
+            var rootPath = _env.ContentRootPath;
 
-			// In case you need to inject HttpContext into any classes
-			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            // Code trick to handle local database stored within the solution
+            string conn = Configuration.GetConnectionString("MainConnection");
+            if (conn.Contains("%CONTENTROOTPATH%"))
+            {
+                conn = conn.Replace("%CONTENTROOTPATH%", _env.ContentRootPath);
+            }
 
-			services.AddTransient<IOrdersViewFunctions, OrdersViewFunctions>();
-			services.AddTransient<IHomeViewFunctions, HomeViewFunctions>();
-			services.AddTransient<IMenuListingViewFunctions, MenuListingViewFunctions>();
+            DatabaseAccess.ServiceCollectionExtensions.RegisterDataServices(services, Configuration, conn);
 
-			services.AddControllersWithViews(); // MVC controllers
-		}
+            // In case you need to inject HttpContext into any classes
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app)
-		{
-			// we are not going to use the default exception page
-			//if (_env.IsDevelopment())
-			//{
-			//	app.UseDeveloperExceptionPage();
-			//}
+            // Register API function services
+            services.AddTransient<IOrdersFunctions, OrdersFunctions>();
+            services.AddTransient<IUsersFunctions, UsersFunctions>();
+            services.AddTransient<IMenuListingsFunctions, MenuListingsFunctions>();
 
-			// Enable the Exception Handler Middleware to catch unhandled exceptions
-			app.UseExceptionHandler("/Error/Unhandled");
-			// Enable Exception Handler to handle HTTP status code errors    
-			app.UseStatusCodePagesWithReExecute("/Error/Status", "?statusCode={0}");
+            services.AddControllers(); // API controllers
 
-			app.Use(async (context, next) =>
-			{
-				if (context.Request.Cookies.ContainsKey("X-Access-Token"))
-				{
-					var accessToken = context.Request.Cookies["X-Access-Token"];
-					if (!context.Request.Headers.ContainsKey("Authorization"))
-					{
-						context.Request.Headers.Add("Authorization", "Bearer " + accessToken);
-					}
-					//context.Request.Headers.Add("Authorization", "Bearer " + accessToken);
-				}
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
 
-				await next();
-			});
+                // Add security definition; adds JWT option at top of swagger display
+                c.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
 
-			// OWASP
-			app.UseHsts();
-			app.UseHttpsRedirection();
+                // Add security requirement; tells swagger to add authorization header to each endpoint
+                // NOTE: not all endpoints need the authorization header, so it will be ignored by the endpoint
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                            Reference = new OpenApiReference
+                            {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+        }
 
-			app.UseStaticFiles();
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            // --- Auto-apply EF Core migrations to ensure tables exist ---
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<DatabaseAccess.Data.Context.MainAppDbContext>();
+                db.Database.Migrate();
+            }
+            // ----------------------------------------------------------
 
-			app.UseRouting();
+            if (_env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
 
-			app.UseAuthentication();
-			app.UseAuthorization();
+                // Enable middleware to serve generated Swagger as a JSON endpoint.
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+            }
 
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllerRoute(
-					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
-				endpoints.MapControllers();
-			});
-		}
-	}
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Cookies.ContainsKey("X-Access-Token"))
+                {
+                    var accessToken = context.Request.Cookies["X-Access-Token"];
+                    if (!context.Request.Headers.ContainsKey("Authorization"))
+                    {
+                        context.Request.Headers.Add("Authorization", "Bearer " + accessToken);
+                    }
+                    //context.Request.Headers.Add("Authorization", "Bearer " + accessToken);
+                }
+
+                await next();
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
